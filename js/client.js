7772fb27e09f6e2445b84c4b36b3490112dab7a0
Nick Saeger <nsaeger1@my.wctc.edu>
2021-02-02 09:59:10 -0600

    week 2

---
+++ js/client.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    // preload audio
+    var toast = new Audio('toast.wav');
+
+    $('.code').on('click', function (e) {
+        e.preventDefault();
+        toast.pause();
+        toast.currentTime = 0;
+        toast.play();
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
+        $('#toast').toast({autohide: false}).toast('show');
+        $('body').on('keyup', (function (event) {
+            if (event.which == 27) {
+                $('.toast').toast('dispose')
+                $('body').off('keyup')
+            }
+        }))
+    });
+
+
+});
